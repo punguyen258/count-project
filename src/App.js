108bff806444126be5f2bdd3d057a356ccc6dcae
Nguyen Ha Phuong <phuong@m.ibis.studio>
2021-02-02 09:33:54 +0700

    Count render component using useRef

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Counter from './Components/Counter';
 import Button from './Components/Button';
 import './App.css';
 
 const App = () => {
+  const counter = useRef(0);
   const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+  const increment = () => {
+    setCount(count + 1);
+    counter.current += 1;
+    console.log(`Count render : ${counter.current}`);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+    counter.current += 1;
+    console.log(`Count render : ${counter.current}`);
+  };
   console.log('App');
 
   return (
     <div className="App">
-      <Button onSubmit={decrement} text="DECREMENT" />
-      <Counter count={count} />
-      <Button onSubmit={increment} text="INCREMENT" />
+      <div className="d-flex">
+        <Button onSubmit={decrement} text="DECREMENT" />
+        <Counter count={count} />
+        <Button onSubmit={increment} text="INCREMENT" />
+      </div>
     </div>
   );
 };
